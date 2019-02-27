5a15898479f3068b7a510cb895a587c3a351e76f
HenriMeng <henri.meng@gmail.com>
2019-02-27 11:55:06 +0100

    first commit

---
+++ Addition/Addition/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addition
+{
+    class Program
+    {
+
+        // Variables pour tous
+        const int min = 1;
+        const int max = 10;
+        static Random random = new Random();
+
+        // Apréciations
+        private static void AfficherAppréciations(int points, int nbQ)
+        {
+            if (points == nbQ)
+            {
+                Console.WriteLine("Vous avez " + points + "points, BRAVO !!");
+            }
+            else if (points > (nbQ / 2))
+            {
+                Console.WriteLine("Vous avez " + points + "points, PAS MAL :)");
+            }
+            else
+            {
+                Console.WriteLine("Vous avez " + points + "points, peut mieux faire :/");
+            }
+        }
+
+  
+
+        // Nouveau type (plus clair)
+        enum OPERATEUR
+        {
+            ADDITION,
+            MULTIPLICATION,
+            SOUSTRACTION
+        }
+
+        // + * -
+        private static OPERATEUR GetOperateur()
+        {
+            int operateur = random.Next(1, 4);
+            if (operateur == 1)
+            {
+                return OPERATEUR.ADDITION;
+            }
+            else if (operateur == 2)
+            {
+                return OPERATEUR.MULTIPLICATION;
+            }
+            return OPERATEUR.SOUSTRACTION;
+        }
+
+        // a + b = ?
+        private static bool DemanderAddition()
+        {
+
+            int a = random.Next(min, max);
+            int b = random.Next(min, max);
+            OPERATEUR operateur = GetOperateur();
+            int resultatFinal = 0;
+
+            // Boucle infinie
+            while (true)
+            {
+                // Calculs cas par cas
+                switch (operateur)
+                {
+                    case OPERATEUR.ADDITION:
+                        {
+                            Console.Write(a + "+" + b + "= ");
+                            resultatFinal = a + b;
+                        }
+                        break;
+                    case OPERATEUR.MULTIPLICATION:
+                        {
+                            Console.Write(a + "x" + b + "= ");
+                            resultatFinal = a * b;
+                        }
+                        break;
+                    case OPERATEUR.SOUSTRACTION:
+                        {
+                            if (a < b)
+                            {
+                                int temp = a;
+                                a = b;
+                                b = temp;
+                            }
+                            Console.Write(a + "-" + b + "= ");
+                            resultatFinal = a - b;
+                        }
+                        break;
+                    default:
+                        {
+                            Console.WriteLine("ERREUR");
+                            Environment.Exit(0);
+                        }
+                        break;
+                }
+                
+                // Compare la réponse vs résultat
+                String resultat = Console.ReadLine();
+                int resultatNum = 0;
+                if (int.TryParse(resultat, out resultatNum))
+                {
+                    if (resultatNum == resultatFinal)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ATTENTION, vous devez entrer un nombre.");
+                }
+            }
+        }
+        
+
+
+        static void Main(string[] args)
+        {
+            const int nbQ = 5;
+            int points = 0;
+
+            // Boucler 5 fois.
+            for (int i = 1; i <= nbQ; i++)
+            {
+                Console.WriteLine("Question " + i + "/" + nbQ + " : ");
+                if (DemanderAddition())
+                {
+                    points++;
+                }
+                Console.WriteLine("");
+
+            }
+
+            AfficherAppréciations(points, nbQ);
+           
+        }
+    }
+}
---
+++ nb_magique_console/nb_magique_console/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nb_magique_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            // étape 2 : Utiliser des constantes pour le min/max
+            const int min = 1;
+            const int max = 10;
+            const int nbDeVies = 3;
+            int vie = 0;
+
+            // étape 1 : Rendre le nombre magique aléatoire
+            Random random = new Random();
+            int nbMagique = random.Next(min, max);
+
+            
+
+            /* étape 4 : Gérer les cas d'erreur où l'utilisateur a rentré
+            un nombre en dehors de min et max*/
+            for (vie = nbDeVies; vie > 0; vie--)
+            {
+                // étape 3 : Afficher "Entre 1 et 10"
+                Console.Write("Devinez le nombre magique entre " + min + " et " + max + " (il vous reste " + vie + ") :");
+                String nb = Console.ReadLine();
+                int nbNum = 0;
+                if (int.TryParse(nb, out nbNum))
+                {
+                    if (nbNum < min)
+                    {
+                        Console.WriteLine("Erreur, votre nombre est plus petit que le min");
+                        vie++;
+                    }
+                    else if (nbNum > max)
+                    {
+                        Console.WriteLine("Erreur, votre nombre est plus grand que le max");
+                        vie++;
+                    }
+                    else
+                    {
+                        if (nbNum < nbMagique)
+                        {
+                            Console.WriteLine("Le nombre magique est plus grand");
+                        }
+                        else if (nbNum > nbMagique)
+                        {
+                            Console.WriteLine("Le nombre magique est plus petit");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Bravo, vous avez trouvé le nombre magique");
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Erreur, vous devez entrer un nombre");
+                }
+            }
+
+            // étape 5 : Désolé vous avez perdu, le nombre magique était x
+            if (vie == 0)
+            {
+                Console.WriteLine("Désolé vous avez perdu, le nombre magique était " + nbMagique);
+            }
+            
+
+
+        }
+    }
+}
